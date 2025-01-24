2b1576ddd85cf27dd924a2662d3d462452f22d7d
Rahul Mahato <rahul.mahato@cloudkaptan.com>
2025-01-24 17:17:37 +0530

    commit: custom lead converter

---
+++ force-app/main/default/lwc/lwcLeadCovertComponent/lwcLeadCovertComponent.js
@@ -0,0 +1,15 @@
+import { LightningElement, api, track } from 'lwc';
+import convertLead from '@salesforce/apex/LeadConversionController.convertLead';
+
+export default class LwcLeadCovertComponent extends LightningElement {
+    @api recordId; 
+    @track message;
+
+    handleConvertLead(){
+        convertLead({leadId: this.recordId}).then((result)=> {
+            this.message = result;
+        }).catch((error)=>{
+            this.message = 'Error: ' + error.body.message;
+        });
+    }
+}
